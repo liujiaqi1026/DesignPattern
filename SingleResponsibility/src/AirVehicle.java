02ca6535c0ac469b5a0a3c2a40d5dbd8aaf3d474
liujiaqi <liujiaqi@e-medclouds.com>
2023-01-31 10:39:24 -0500

    单一原则

---
+++ SingleResponsibility/src/AirVehicle.java
@@ -0,0 +1,6 @@
+public class AirVehicle implements Vehicle{
+    @Override
+    public void run(String vehicle) {
+        System.out.println(vehicle + " run in the air.");
+    }
+}
---
+++ SingleResponsibility/src/RoadVehicle.java
@@ -0,0 +1,7 @@
+public class RoadVehicle implements Vehicle{
+
+    @Override
+    public void run(String vehicle) {
+        System.out.println(vehicle + " run in the road.");
+    }
+}
